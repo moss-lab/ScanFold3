07c9250faca6c5802bf4d96edaad27b67d73e4a2
Evelyn <31992045+EthanCC-1@users.noreply.github.com>
2024-11-05 12:22:21 -0600

    Delete src/fold_wrapper.hpp

--- src/fold_wrapper.hpp
+++
@@ -1,9 +0,0 @@
-#include "fold.hpp"
-#include <pybind11/pybind11.h>
-namespace py = pybind11;
-//python bindings
-PYBIND11_MODULE(fold, var)
-{
-    var.doc() = "Classes and functions needed for ScanFold-Fold";
-
-}
