6b3c766796b2fbdbb3df5454d22345166d0614d9
Evelyn <31992045+EthanCC-1@users.noreply.github.com>
2025-01-28 11:21:44 -0600

    Add files via upload

--- run.sh
+++ run.sh
@@ -6,10 +6,10 @@
 #SBATCH --job-name=scanfold3_test   #Name of this job in work queue
 #SBATCH --mail-user=  #Email to send notifications to
 #SBATCH --mail-type=ALL             #Email notification type (BEGIN, END, FAIL, ALL)
-#SBATCH --mem=25g
+#SBATCH --mem=400g
 
 cd test/run;
 conda activate ScanFold3;
-python test.py;
+../test/test.o /work/LAS/wmoss-lab/ecoppen/benchmarks/scanfold3_w_flowgraph/test/benchmark/trimmed_EBV;
 wait;
 
